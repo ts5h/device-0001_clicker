47ca5e96858068c924487b5f5d3689162a7e2ac5
ts5h <at4@jc4.so-net.ne.jp>
2021-02-05 23:09:45 +0100

    Files sorting

--- build/default/production/mcc_generated_files/device_config.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/mcc_generated_files/device_config.d  \
- build/default/production/mcc_generated_files/device_config.p1:  \
- mcc_generated_files/device_config.c  \
-C:/Program\ Files/Microchip/MPLABX/v5.45/packs/Microchip/PIC12-16F1xxx_DFP/1.2.63/xc8/pic/include/language_support.h 
